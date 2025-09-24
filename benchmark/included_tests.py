ade5e0c99816bcd2c25d93a3586512d0036aac2b
NMANISH9800 <96011091+NMANISH9800@users.noreply.github.com>
2025-09-24 19:42:45 +0530

    Delete benchmark directory

--- benchmark/included_tests.py
+++
@@ -1,9 +0,0 @@
-INCLUDED_CONSTRAINT_TESTS = [
-    ('numerical', 'FixedIncrements'),
-    ('categorical', 'FixedCombinations'),
-    ('boolean', 'FixedCombinations'),
-    ('numerical', 'Inequality'),
-    ('datetime', 'Inequality'),
-    ('numerical', 'Range'),
-    ('datetime', 'Range'),
-]
